3759bbb5bc66485e08c266d70845b1e093e05e60
Payam <p.babaee@live.com>
2019-01-15 04:48:28 +0000

    add missing test for FibonacciCalculatorWorkerCommand

--- tests/Feature/FibonacciCalculatorWorkerCommandTest.php
+++ tests/Feature/FibonacciCalculatorWorkerCommandTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Tests\Feature;
 
 use App\Console\Commands\FibonacciCalculatorWorkerCommand;
+use App\Exceptions\InvalidStateException;
 use App\Service\Fibonacci\WriterService;
 use Illuminate\Config\Repository;
 use Tests\TestCase;
@@ -32,8 +33,8 @@ class FibonacciCalculatorWorkerCommandTest extends TestCase
         $writerServiceStub->method('canContinue')->willReturn(true);
 
         $writerServiceStub->method('getLastNumbersFromSequence')->willReturn([
-            10 => '55',
-            11 => '89',
+            '55',
+            '89',
         ]);
 
         $writerServiceStub->method('addToSequence')
@@ -48,6 +49,42 @@ class FibonacciCalculatorWorkerCommandTest extends TestCase
         );
 
         $this->expectException(\Exception::class);
+        $fibonacciCalculatorWorkerCommand->handle();
+    }
+
+    /**
+     * @author PB
+     */
+    public function testFibonacciCaluculationInvalidState()
+    {
+        $mockConfigRepository = $this->getMockBuilder(Repository::class)->getMock();
+        $mockConfigRepository->method('get')
+            ->willReturn(0);
+
+        $writerServiceStub = $this->getMockBuilder(WriterService::class)
+            ->setMethods(['initialiseSequence', 'addToSequence', 'canContinue', 'getLastNumbersFromSequence'])
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $writerServiceStub->method('canContinue')->willReturn(true);
+
+        $writerServiceStub->method('getLastNumbersFromSequence')->willReturn([
+            '95',
+            '89',
+        ]);
+
+        $writerServiceStub->method('addToSequence')
+            ->with(144)
+            ->willThrowException(new \Exception('exiting'));
+
+        $fibonacciCalculatorWorkerCommand = \Mockery::mock(
+            'App\Console\Commands\FibonacciCalculatorWorkerCommand[]', [
+                $writerServiceStub,
+                $mockConfigRepository
+            ]
+        );
+
+        $this->expectException(InvalidStateException::class);
         $fibonacciCalculatorWorkerCommand->handle();
     }
 }
